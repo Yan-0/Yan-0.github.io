9937e0c1e329e53fd3a39abc4fa080a754630d0b
Shreyan <shreyanbhandari36@gmail.com>
2024-03-09 17:59:58 +0545

    updates made

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function toggleProjectDetails(projectId) {
+    const dropdownContent = document.getElementById(projectId);
+    dropdownContent.classList.toggle('show');
+}
+
+function toggleCertificationDetails(certificationId) {
+    const dropdownContent = document.getElementById(certificationId);
+    dropdownContent.classList.toggle('show');
+}
